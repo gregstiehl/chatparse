5aba5a7a416c00687bc97ca65b7eb19527cdab9d
Greg Stiehl <greg@stiehl.com>
2015-02-12 17:53:49 -0600

    parse @mentions using regex

--- chatparse.py
+++ chatparse.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+import re
+
+re_mentions = re.compile('(?<=@)\w+')
+
 def parse(msg):
-    return ''
+    contents = {}
+    contents['mentions'] = re_mentions.findall(msg)
+    return contents
+
+if __name__ == '__main__':
+    result = parse('@test@1 @greg @aaa')
+    print(result)
