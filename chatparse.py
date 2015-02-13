fe8ef1157675324f7cfc05f2046d8c0c7c21b06d
Greg Stiehl <greg@stiehl.com>
2015-02-12 18:28:38 -0600

    parse (emoticons) using regex

--- chatparse.py
+++ chatparse.py
@@ -3,12 +3,18 @@
 import re
 
 re_mentions = re.compile('(?<=@)\w+')
+re_emoticons = re.compile('\((\w+)\)')
 
 def parse(msg):
     contents = {}
-    contents['mentions'] = re_mentions.findall(msg)
+    mentions = re_mentions.findall(msg)
+    if mentions:
+        contents['mentions'] = mentions
+    emoticons = re_emoticons.findall(msg)
+    if emoticons:
+        contents['emoticons'] = emoticons
     return contents
 
 if __name__ == '__main__':
-    result = parse('@test@1 @greg @aaa')
+    result = parse('@test@1 @greg @aaa (smile)')
     print(result)
